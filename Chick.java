e2195f6f78a9f34822b6b82b47e950a0e0adca90
unknown <lili10@697-334-Edoras.sf.k12.internal>
2015-11-17 08:09:17 -0800

    Added first name to index.html

--- Chick.java
+++ Chick.java
@@ -1,4 +1,29 @@
 class Chick implements Animal 
-{     
-     //your code here
+{        
+     private String myType;     
+     private String mySound;  
+     public Chick(String type, String sound) 
+     {         
+         myType = type;         
+         mySound = sound;     
+     }         
+     public Chick(String type, String sound1, String sound2)
+     {
+     	 myType = type;
+	     if(Math.random()*1 < .5)
+	     {
+	     	mySound = sound2;
+	     }      
+	     else
+	     {
+	         mySound = sound1;
+	     }
+	 }
+     public Chick()     
+     {         
+         myType = "unknown";         
+         mySound = "unknown";  
+     } 
+    public String getSound() {return mySound;}  
+    public String getType() {return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,6 @@
 
-void setup()
-{
-	//your variable declarations here
-}
-
+public void setup() 
+{     
+   Farm barnyard = new Farm();
+   barnyard.animalSounds();
+}  
